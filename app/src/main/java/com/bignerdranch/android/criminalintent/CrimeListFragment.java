3a6932a935f93f0c1ead3471f37b8e63856c01c8
treetender <treetender@noreply.github.com>
2015-01-27 12:04:41 -0400

    Fixed up issues with ActionBarActivity

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -96,7 +96,7 @@ public class CrimeListFragment extends ListFragment {
                 return true;
             case R.id.menu_item_show_subtitle:
                 if(((ActionBarActivity)getActivity()).getSupportActionBar().getSubtitle() == null) {
-                    getActivity().getActionBar().setSubtitle(R.string.subtitle);
+                    ((ActionBarActivity)getActivity()).getSupportActionBar().setSubtitle(R.string.subtitle);
                     mSubtitleVisible = true;
                     item.setTitle(R.string.hide_subtitle);
                 }
