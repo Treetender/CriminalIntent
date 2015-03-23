c3ea2239d799063f01f90381ee7a7e9a76b04686
treetender <treetender@noreply.github.com>
2015-03-22 23:22:38 -0300

    Added imageview and we now show the picture in the preview

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -15,11 +15,13 @@ public class Crime {
     private static final String JSON_TITLE = "title";
     private static final String JSON_SOLVED = "solved";
     private static final String JSON_DATE = "date";
+    private static final String JSON_PHOTO = "photo";
 
     private UUID mId;
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private Photo mPhoto;
 
     public Crime() {
         //Generate Unique Identifier
@@ -33,6 +35,8 @@ public class Crime {
             mTitle = json.getString(JSON_TITLE);
         mSolved = json.getBoolean(JSON_SOLVED);
         mDate = new Date(json.getLong(JSON_DATE));
+        if(json.has(JSON_PHOTO))
+            mPhoto = new Photo(json.getJSONObject(JSON_PHOTO));
     }
 
     public UUID getId() {
@@ -55,6 +59,14 @@ public class Crime {
         mDate = date;
     }
 
+    public Photo getPhoto() {
+        return mPhoto;
+    }
+
+    public void setPhoto(Photo p) {
+        mPhoto = p;
+    }
+
     public boolean isSolved() {
         return mSolved;
     }
@@ -75,6 +87,8 @@ public class Crime {
         json.put(JSON_TITLE, mTitle);
         json.put(JSON_SOLVED, mSolved);
         json.put(JSON_DATE, mDate.getTime());
+        if (mPhoto != null)
+            json.put(JSON_PHOTO, mPhoto.toJSON());
 
         return json;
     }
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -24,6 +25,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -37,6 +39,7 @@ public class CrimeFragment extends Fragment {
     private EditText mTitleField;
     private Button mDateButton, mTimeButton;
     private ImageButton mCameraButton;
+    private ImageView mPhotoView;
     private CheckBox mSolvedCheckBox;
 
     private static final String TAG = "CrimeFragment";
@@ -141,6 +144,8 @@ public class CrimeFragment extends Fragment {
             }
         });
 
+        mPhotoView = (ImageView)v.findViewById(R.id.crime_imageView);
+
         PackageManager pm = getActivity().getPackageManager();
         boolean hasCamera = pm.hasSystemFeature(PackageManager.FEATURE_CAMERA)
                          || pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT)
@@ -191,10 +196,18 @@ public class CrimeFragment extends Fragment {
         } else if (requestCode == REQUEST_PHOTO) {
             //Create a new photo and attach it
             String filename = data.getStringExtra(CrimeCameraFragment.EXTRA_PHOTO_FILENAME);
-            if (filename != null)
-                Log.i(TAG, "filename: " + filename);
+            if (filename != null) {
+                Photo p = new Photo(filename);
+                mCrime.setPhoto(p);
+                showPhoto();
+            }
         }
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        showPhoto();
     }
 
     private void updateDate() {
@@ -203,5 +216,17 @@ public class CrimeFragment extends Fragment {
 
     private void updateTime() {
         mTimeButton.setText(DateFormat.format(FORMAT_TIME, mCrime.getDate()));
+    }
+
+    private void showPhoto() {
+        //(Re)set the image button's image based on our photo
+        Photo p = mCrime.getPhoto();
+        BitmapDrawable b = null;
+
+        if (p != null) {
+            String path = getActivity().getFileStreamPath(p.getFilename()).getAbsolutePath();
+            b = PictureUtils.getScaledDrawable(getActivity(), path);
+        }
+        mPhotoView.setImageDrawable(b);
     }
 }
